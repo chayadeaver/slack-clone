918574084267317bb72bc1bc9c611478691952ea
chayadeaver <chayadeaver@gmail.com>
2020-11-02 11:01:06 -0500

    added another sidebar option

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,6 +11,7 @@ import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 import SidebarOption from './SidebarOption';
 
 function Sidebar() {
@@ -37,6 +38,9 @@ function Sidebar() {
             <hr />
             <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
             <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channels" />
+
+            {/* Connect to db and list all the channels */}
         </div>
     )
 }
