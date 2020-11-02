e177e10487ac05d5bf99f27c198fff0e896381aa
chayadeaver <chayadeaver@gmail.com>
2020-11-01 21:31:39 -0500

    changed file imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World </h1>
+      <h1>Welcome to Chaya's Slack Clone!</h1>
 
       <Header />
       <div className="app__body">
