696edab5a75f0bf85b4bda529a3f3e2db61da66f
chayadeaver <chayadeaver@gmail.com>
2020-11-07 14:57:44 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Chat from './components/chat/Chat'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Chaya's Slack Clone!</h1>
-
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* React-Router -> */} 
-      </div>
-      
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+          {/* React-Router -> Chat screen */} 
+        </div>
+      </Router>
     </div>
   );
 }
